59b7711bbe4430cf26c919e9994e00017150b3be
Vincent Chau <vichau@ucdavis.edu>
2016-11-01 19:20:13 -0700

    hooked up tableview and storyboard views

--- WishList/WishList/HomeVC.swift
+++ WishList/WishList/HomeVC.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class HomeVC: UIViewController {
+class HomeVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var itemTableView: UITableView!
+    @IBOutlet weak var categorySegmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let item = Item()
+        itemTableView.delegate = self
+        itemTableView.dataSource = self
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,14 @@ class HomeVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+   
+        return cell
+    }
 }
 
---
+++ WishList/WishList/ItemTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ItemTableViewCell.swift
+//  WishList
+//
+//  Created by Vincent Chau on 11/1/16.
+//  Copyright © 2016 Vincent Chau. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var itemDescriptionLabel: UILabel!
+    @IBOutlet weak var itemPriceLabel: UILabel!
+    @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var itemImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
