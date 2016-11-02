b23f02281e1f7a6dc866ba46c8034839e24082a8
Vincent Chau <vichau@ucdavis.edu>
2016-11-01 21:08:55 -0700

    coredata setup for tableview

--- WishList/WishList/HomeVC.swift
+++ WishList/WishList/HomeVC.swift
@@ -7,32 +7,110 @@
 //
 
 import UIKit
+import CoreData
 
-class HomeVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class HomeVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
 
     @IBOutlet weak var itemTableView: UITableView!
     @IBOutlet weak var categorySegmentedControl: UISegmentedControl!
+    
+    var fetchedResultsController: NSFetchedResultsController<Item>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         itemTableView.delegate = self
         itemTableView.dataSource = self
-    
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+      
     }
 
     // MARK: - UITableViewDataSource
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
    
         return cell
+    }
+    
+    // MARK: - Helpers
+    func attemptFetch() {
+        
+        // Create fetch request
+        let fetchRequest: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        // Sort
+        let recentSort = NSSortDescriptor(key: "created", ascending: false)
+        
+        fetchRequest.sortDescriptors = [recentSort]
+        
+        // FRC Controller
+        let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        self.fetchedResultsController = controller
+        
+        // Perform fetch
+        do {
+            
+            try self.fetchedResultsController.performFetch()
+        
+        } catch {
+        
+            let error = error as NSError
+            print("\(error)")
+        
+        }
+        
+    }
+    
+    // When table view updates, listen for changes and handle it
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        itemTableView.beginUpdates()
+        
+        
+    }
+    
+    // Once table view did change
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        itemTableView.endUpdates()
+    }
+    
+    // Listen for when making a change (insert,delete,modification) NSFetchedResultsChagneType
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .insert:
+            if let indexPath = newIndexPath {
+                itemTableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break
+        case .delete:
+            if let indexPath = indexPath {
+                itemTableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            break
+            
+        case .update:
+            if let indexPath = indexPath {
+                let cell = itemTableView.cellForRow(at: indexPath) as! ItemTableViewCell
+                // update cell data
+            }
+            break
+        case .move:
+            if let indexPath = indexPath {
+                itemTableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            if let indexPath = newIndexPath {
+                itemTableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break
+        }
     }
 }
 
--- WishList/WishList/ItemTableViewCell.swift
+++ WishList/WishList/ItemTableViewCell.swift
@@ -22,7 +22,6 @@ class ItemTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         
     }
 
